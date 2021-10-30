2a6b6304ce5f654e484e965bec31967946f913d6
Zaklaria <zsumonn@gmail.com>
2021-10-31 00:23:57 +0600

    first commit

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+const errorMiddleware = require('./middleware/error');
+
+// Config
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+	require('dotenv').config({ path: 'backend/config/config.env' });
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(cors());
+
+// Route Imports
+const product = require('./routes/productRoute');
+const user = require('./routes/userRoute');
+const order = require('./routes/orderRoute');
+const payment = require('./routes/paymentRoute');
+
+app.use('/api/v1', product);
+app.use('/api/v1', user);
+app.use('/api/v1', order);
+app.use('/api/v1', payment);
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'Welcome My Dream Shop Api' });
+});
+
+// Middleware for Errors
+app.use(errorMiddleware);
+
+module.exports = app;
